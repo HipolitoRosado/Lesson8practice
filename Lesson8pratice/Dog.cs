3cc32962fb1721bd292f49223b867dceb1403131
HipolitoRosado <paulserrano58@gmail.com>
2020-03-10 12:58:43 -0500

    Add project files.

---
+++ Lesson8pratice/Dog.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson8pratice
+{
+    class Dog : Animal
+    {
+        private string favoriteChewToy;
+
+        public Dog(string name, int numOfLegs, int numOfArms, bool isDangerous, bool isFury, string favoriteChewToy)
+
+        {
+            //stuff from our base class(parent) Animal
+            this.name = name;
+            this.numOfLegs = numOfLegs;
+            this.numOfArms = numOfArms;
+            this.isDangerous = isDangerous;
+            this.isFurry = isFury;
+
+            //this is how i can add new stuff my dog can do
+            this.favoriteChewToy = favoriteChewToy;
+
+        }
+
+        public void Bark()
+        {
+            Console.WriteLine("The " + this.GetType() + " goes WOOF WOOF!");
+        }
+
+        public override int Sell()
+        {
+            Console.WriteLine("I love my dog and would never sell it");
+            return 0;
+        }
+
+    }
+
+}
---
+++ Lesson8pratice/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson8pratice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog myDog = new Dog("Fido", 4, 0, false, true, "Dog Bone");
+            myDog.Bark();
+            myDog.Eat();
+            myDog.Sleep();
+
+        }
+    }
+    
+
+
+}
